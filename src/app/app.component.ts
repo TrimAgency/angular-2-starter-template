41e6047903db8adfcf0fa14ec20e36f73a7733a5
Dominique Miller <dominique.p.miller@gmail.com>
2017-03-06 22:16:58 -0500

    Add const API_URL for dev and production evnironments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { API_URL } from '../environments/environment'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = API_URL;
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
   production: true
 };
+
+export const API_URL = 'your apps url here';
