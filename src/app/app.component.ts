a91ed30579d4f0e28f4ffc7214d57f716d6935ad
Dominique Miller <dominique.p.miller@gmail.com>
2017-03-17 13:17:11 -0400

    Add title service to set document title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title: string;
 
-  constructor() {
+  constructor( public titleService: Title) {}
+
+  ngOnInit () {
     this.title = 'Welcome to the Trim Angular 2 starter template';
+
+    this.titleService.setTitle(this.title);
   }
+
 }
