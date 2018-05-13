f19cf556b559b0276ec0f5787ac2e44081f1eff7
sgilliam15 <stevengilliamjr@gmail.com>
2018-05-12 21:36:31 -0400

    update rxjs 5 -> 6

--- src/app/services/auth.interceptor.ts
+++ src/app/services/auth.interceptor.ts
@@ -1,4 +1,4 @@
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest} from '@angular/common/http';
--- src/app/services/response.interceptor.ts
+++ src/app/services/response.interceptor.ts
@@ -1,9 +1,10 @@
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/observable/throw';
+
+import { throwError as observableThrowError,  Observable } from 'rxjs';
+import { tap, catchError} from 'rxjs/operators';
+
+
 import { Injectable } from '@angular/core';
-import { HttpEvent, 
+import { HttpEvent,
          HttpInterceptor,
          HttpHandler,
          HttpRequest,
@@ -16,22 +17,21 @@ import { AuthService } from './auth.service';
 export class ResponseInterceptor implements HttpInterceptor {
   constructor( private auth: AuthService ) {}
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<HttpResponse<any>>> {
-    return next.handle(req)
-               .do( event => {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+               tap( event => {
                  if (event instanceof HttpResponse ) {
                    // place your response logic here
                  }
-               } )
-               .catch(response => {
+               }),
+               catchError(response => {
                  if (response instanceof HttpErrorResponse && response.status === 401 ) {
                   // 401 unauthorized logic here
                   console.log('401', response);
                   this.auth.logout();
                  }
 
-                return Observable.throw(response);
-                });
-
+                return observableThrowError(response);
+                }));
   }
 }
